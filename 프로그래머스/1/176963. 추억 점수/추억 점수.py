3676daa0f96762c66858b6999b135214d68f1a32
ASJ0211 <118821779+ASJ0211@users.noreply.github.com>
2025-09-25 01:01:55 +0900

    [level 1] Title: 추억 점수, Time: 2.66 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,17 +1,41 @@
 def solution(name, yearning, photo):
-    answer = []
-    dic={}
-    c=0
-
-    for i in name:
-        dic[i]=yearning[c]
-        c+=1
+    d=dict(zip(name,yearning))
+    
+    answer=list()
+   
     for i in photo:
-        r=0
+        ans=0
         for j in i:
-            if j not in dic:
-                dic[j]=0
-            r+=dic[j]
-            print(r)
-        answer.append(r)
+            try:
+                ans+=d[j]
+                #ans += d.get(j, 0)
+                
+            except:
+                continue
+        answer.append(ans)
     return answer
+
+
+
+
+
+
+
+
+# def solution(name, yearning, photo):
+#     answer = []
+#     dic={}
+#     c=0
+
+#     for i in name:
+#         dic[i]=yearning[c]
+#         c+=1
+#     for i in photo:
+#         r=0
+#         for j in i:
+#             if j not in dic:
+#                 dic[j]=0
+#             r+=dic[j]
+#             print(r)
+#         answer.append(r)
+#     return answer
