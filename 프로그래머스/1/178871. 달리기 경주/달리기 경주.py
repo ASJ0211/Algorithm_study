2ae5cfe91a5b9b337a8543e1ea4f39b37a396535
ASJ0211 <118821779+ASJ0211@users.noreply.github.com>
2025-09-24 22:48:55 +0900

    [level 1] Title: 달리기 경주, Time: 1592.35 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,11 +1,33 @@
-def solution(players, callings):
-    dic = {}
-    for rank,name in enumerate(players):
-        dic[name] = rank
-    for i in callings:
-        cur_rank = dic[i]
-        players[cur_rank],players[cur_rank-1]=players[cur_rank-1],players[cur_rank]
+# def solution(players, callings):
+#     for i in callings:
+#         for index,p in enumerate(players):
+#             if i ==p:
+#                 players[index-1],players[index] = players[index],players[index-1]
+#                 print()
+#                 break    
         
-        dic[players[cur_rank]] = cur_rank
-        dic[players[cur_rank-1]] = cur_rank-1
-    return players
+#     answer = players
+#     return answer
+
+#dict로 해서 바로 찾는 접근법으로 해야할듯
+#dict를 2개를 해야하나? 
+# gpt 
+#앞사람은 index - 1 로 바로 접근할 수 있어요.
+#그래서 dict랑 list를 같이 관리하면 O(1) 안에 해결됩니다 ✅ 라네요.
+
+def solution(players, callings):
+    d=dict()
+    l=list()
+    for i,p in enumerate(players):
+        d[p]=i
+        l.append(p)
+        
+    for c in callings:
+        f=d[c]
+        before= l[f-1]
+        d[c]-=1
+        d[before]+=1
+        l[f],l[f-1]=l[f-1],l[f]
+
+        
+    return l
