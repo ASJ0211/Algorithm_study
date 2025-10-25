ad5c59bd43187a1a3b182fbbd7e012b0712f6b70
ASJ0211 <118821779+ASJ0211@users.noreply.github.com>
2025-10-25 14:48:12 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,4 +1,20 @@
-select info.id,name.fish_name,info.length
+# select ta.id,tb.fish_name, ta.length
+# from fish_info as ta join fish_name_info as tb on ta.fish_type = tb.fish_type
+# where ta.length in (select max(length) as mleng from fish_info group by fish_type having length = max(length))
+# order by ta.id asc
+
+
+
+
+
+
+
+
+
+
+
+
+select info.id,name.fish_name,info.length as length
 from fish_info as info join fish_name_info as name on info.fish_type = name.fish_type
 where info.fish_type in 
     (select fish_type 
